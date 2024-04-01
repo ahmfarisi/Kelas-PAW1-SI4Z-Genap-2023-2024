360c921c604b2be5888e1a6c7fd035feb94cb0c4
Nur Rachmat <>
2024-04-01 10:04:06 +0700

    Create intro_to_php.php

---
+++ 06_php/01_intro/intro_to_php.php
@@ -0,0 +1,21 @@
+<?php
+    $name = "Ahmad Farisi";
+    $age = 17;
+    $height = 178.5;
+
+    echo "Hello world";
+    echo "<br/>";
+    echo $name;
+    echo "<br/>";
+    echo "Age : $age years old";
+    echo "<br/>";
+    echo "Height : $height cm";
+
+    echo "<br/><br/>";
+
+    var_dump($name);
+    echo "<br/>";
+    var_dump($age);
+    echo "<br/>";
+    var_dump($height);
+?>
