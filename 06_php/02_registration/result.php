23d8b81c4245088ad4bd1fce7910c90bfcaa9056
Nur Rachmat <>
2024-04-03 10:46:36 +0700

    update result.php

--- 06_php/02_registration/result.php
+++ 06_php/02_registration/result.php
@@ -11,7 +11,64 @@
   </head>
 
   <body>
+    <?php
+      $name = $_POST["name"];
+      $gender = $_POST["gender"];
+      $religion = $_POST["religion"];
+      $description = $_POST["description"];
+      // echo $name;
+      // echo "<br/>";
+      // echo $gender;
+      // echo "<br/>";
+      // echo $religion;
+      // echo "<br/>";
+      // echo $description;
+    ?>
     
+    <div class="container">
+        <div class="col-6 mx-auto">
+            <div class="card mt-3">
+                <div class="card-header bg-dark text-white">
+                    <h5>Registration Data</h5>
+                </div>
+                <div class="card-body">
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td>&nbsp; : &nbsp; </td>
+                            <td>
+                              <?php echo $name; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Gender</td>
+                            <td>&nbsp; : &nbsp; </td>
+                            <td>
+                              <?php echo $gender; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Religion</td>
+                            <td>&nbsp; : &nbsp; </td>
+                            <td>
+                                <?php echo $religion; ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Self Description</td>
+                            <td>&nbsp; : &nbsp; </td>
+                            <td>
+                                <?php echo $description; ?>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="card-footer d-grid">
+                    <a href="index.php" class="btn btn-outline-dark">Back</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   </body>
